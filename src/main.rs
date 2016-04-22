f05f5f93f3022b23347d4f36e192cff2f324121b
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-04-22 21:47:04 +0100

    Implement piston_window's "Hello World"
    * Open a window
    * Clear it in green

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate piston_window;
+
+use piston_window::{PistonWindow, WindowSettings, clear};
+
+struct Palette {
+}
+
+impl Palette {
+    fn green() -> [f32; 4] {
+        [0.5, 1.0, 0.5, 1.0]
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (w, h) = (800, 600); // px
+    let mut window: PistonWindow = WindowSettings::new("Hello, world!", (w, h))
+                                       .exit_on_esc(true)
+                                       .build()
+                                       .unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g| {
+            clear(Palette::green(), g);
+        });
+    }
 }
